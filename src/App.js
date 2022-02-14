9789df0cb31f13df9166f4770b2524baf39bc46b
maora96 <marinaoliveira.r07@gmail.com>
2022-02-14 17:36:32 -0300

    feat: desktop version (html/css).

--- src/App.js
+++ src/App.js
@@ -1,52 +1,76 @@
+import Badge from "./assets/badge.png";
+import PlayButton from "./assets/play-button.png";
+import HeaderImage from "./assets/header-image.png";
+
+import ProfilePic from "./assets/profile-pic.png";
+import Section1Image from "./assets/section1-image.png";
+import Section3Image from "./assets/section3-image.png"
+import Section4Image from "./assets/section4-image.png"
+import Section5Image from './assets/section5-image.png'
+
+import Icon from "./assets/icon.svg"
+import Logo from "./assets/logo.svg"
+import Instagram from "./assets/instagram.png"
+import Twitter from "./assets/twitter.png"
+import Facebook from "./assets/facebook.png"
+import "./App.css";
+
 
 function App() {
   return (
     <div className="App">
       <header>
+        <img src={Badge} className="badge"/>
         <nav>
-          <div className="menu">
-            <a href="">Create Your Nanny Share</a>
-            <a href="">Browse Shares</a>
-            <a href="">Our Story</a>
-          </div>
+            <div className="menu">
+              <a href="">Create Your Nanny Share</a>
+              <a href="">Browse Shares</a>
+              <a href="">Our Story</a>
+            </div>
 
 
-          <div className="call-to-action">
-            <a href="">Become a Nanny Share</a>
-            <a href="">Sign In</a>
-          </div>
-        </nav>
+            <div className="call-to-action">
+              <a href="" className="call">Become a Nanny Share</a>
+              <a href="">Sign In</a>
+            </div>
+          </nav>
 
-        <section className="hero">
-          <div className="content-left">
-            <h1>Easily create or join a local nanny share with Hapu</h1>
-            <span>Hapu is Airbnb for nanny share. Share your home, nanny and costs and create new flexible, affordable solutions in childcare.</span>
+          <section className="hero">
+            <div className="content-left">
+              <h1>Easily create or join a local<br/> nanny share with Hapu</h1>
+              <span>Hapu is Airbnb for nanny share. Share your home, nanny and costs <br/>and create new flexible, affordable solutions in childcare.</span>
 
-            <div className="video">See hapu in action (27 seconds)</div>
-          </div>
+              <div className="video">
+              <img src={PlayButton}/>
+                See hapu in action (27 seconds)
+              </div>
+            </div>
 
-          <div className="content-right">
-            <img src=""/>
-          </div>
+            <div className="content-right">
+              <img src={HeaderImage}/>
+            </div>
 
-        </section>
+          </section>
+       
       </header>
 
 
       <section className="notice">
-        <img src=""/>
-        <a href="">Sarah’s day care available now in North Sydney</a> 
-        <span>Wednesday, Thursday, Friday - 7:30 - 5:30</span>
+        <div className="container">
+          <img src={ProfilePic}/>
+          <a href="">Sarah’s day care available now in North Sydney</a> 
+          <span>Wednesday, Thursday, Friday - 7:30 - 5:30</span>
+        </div>
       </section>
 
       <section className="about">
         <div className="about-content">
-          <h2>Share your home, nanny and costs</h2>
-          <p>You have a fantastic home, a fantastic nanny and wouldn’t cutting your costs in half be, well, fantastic?! If only it was easy to connect with other parents to share your costs? Well now it is, with Hapu. Hapu means tribe and it’s our foundational 3 tribal principles that empowers you to create and manage your own tribe. A tribe that together has the power to create new affordable solutions in childcare that work for you and your community.</p>
+          <h2>Share your home, <br/>nanny and costs</h2>
+          <p>You have a fantastic home, a fantastic nanny and wouldn’t cutting your costs in half be, well, fantastic?! If only it was easy to connect with other parents to share your costs? Well now it is, with Hapu. <a href="">Hapu means tribe</a> and it’s our foundational 3 tribal principles that empowers you to create and manage your own tribe. A tribe that together has the power to create new affordable solutions in childcare that work for you and your community.</p>
           <a href="">Ready to get started?</a>
         </div>
 
-        <img src=""/>
+        <img src={Section1Image}/>
       </section>
 
       <section className="newsletter">
@@ -54,8 +78,8 @@ function App() {
         <span>Leave us your name and email and we’ll update you as soon as a share becomes available in your area!</span>
 
         <form>
-          <input type="text"/>
-          <input type="email"/>
+          <input type="text" placeholder="Your name"/>
+          <input type="email"placeholder="Your email"/>
           <button>Send</button>
         </form>
       
@@ -63,7 +87,7 @@ function App() {
 
 
       <section className="shared-payments">
-        <img src=""/>
+        <img src={Section3Image}/>
 
         <div className="shared-payments-about">
           <h2>Shared payments made simple</h2>
@@ -77,17 +101,21 @@ function App() {
 
       <section className="framework">
 
-        <h2>A framework built for the long term</h2>
+        <div className="framework-content">
+          <h2>A framework built for the long term</h2>
 
-        <p>Childcare is for the long term. And you need a framework you can count on that gives your share long term viability and success. That’s why we’ve defined Hapu around our three tribal principles; clearly defined process, transparency over money and equality of participation.</p>
+          <p>Childcare is for the long term. And you need a framework you can count on that gives your share long term viability and success. That’s why we’ve defined Hapu around our three tribal principles; clearly defined process, transparency over money and equality of participation.</p>
 
-        <a href="">Read how Hapu’s tribal background defines our app </a>
+          <a href="">Read how Hapu’s tribal background defines our app </a>
+        </div>
 
-        <img src=""/>
+        <div className="image">
+          <img src={Section4Image}/>
+        </div>
       </section>
 
       <section className="coming-soon">
-        <img src=""/>
+        <img src={Section5Image}/>
         <h2>Coming soon: Nanny Share Daily Diary!</h2>
         <p>With the Hapu daily diary your nanny will be able to update you and your sharers with photos and commentary of the day. You and sharers will receive notifications and you’ll be able to login to view the daily adventures fo your little ones. We can’t wait!</p>
       </section>
@@ -97,7 +125,7 @@ function App() {
         <p>Takes less than 5 minutes to get started</p>
 
         <div className="button">
-          <img src=""/>
+          <img src={Icon}/>
           <div>
             <span>Create Your Nanny Share</span>
             <span>Takes less than 5 minutes</span>
@@ -107,7 +135,7 @@ function App() {
         <a href="">Or browse local nanny-shares</a>
 
         <nav>
-          <img src=""/>
+          <img src={Logo}/>
 
           <div className="menu">
             <a href="">Share Your Nanny</a>
@@ -117,12 +145,12 @@ function App() {
           </div>
 
           <div className="social-media-links">
-            <a href=""><img src=""/></a>
-            <a href=""><img src=""/></a>
-            <a href=""><img src=""/></a>
+            <a href=""><img src={Facebook}/></a>
+            <a href=""><img src={Twitter}/></a>
+            <a href=""><img src={Instagram}/></a>
           </div>
         </nav>
-        <p>Copyright © 2017 Hapu PTY Limited All rights reserved</p>
+        <p class="copyright">Copyright © 2017 Hapu PTY Limited All rights reserved</p>
       </footer>
 
     </div>
